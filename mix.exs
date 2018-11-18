956fcf110ed34f40a54ed21e039218ab153f9be8
David Dunn <26876072+doubledup@users.noreply.github.com>
2018-11-18 20:53:48 +0200

    Update mix metadata

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule Weather.MixProject do
   def project do
     [
       app: :weather,
+      name: "Weather CLI",
+      source_url: "https://github.com/doubledup/issues-cli",
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
